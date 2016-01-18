bd453e454b9b985a8e0abb052c4c57ee265229f1
Lucas Vieira <newlucasfevi@gmail.com>
2016-01-17 19:54:24 -0600

    Lab1 almost finished, just need to insert the last questions.

---
+++ js/lab1.js
@@ -0,0 +1,7 @@
+function hello()
+{
+	var firstName = prompt("Please enter your first name", "Harry");
+	var lastName = prompt("Please enter your last name", "Potter");
+
+	alert("Hello " + lastName + ", " + firstName + "! Welcome to ITK353.");
+}
