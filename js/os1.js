0f69408ae01da1abc9eff0f4362441b18bbcdd2b
Lucas Vieira <newlucasfevi@gmail.com>
2016-02-10 10:17:50 -0600

    Finished part A of online sessions

---
+++ js/os1.js
@@ -0,0 +1,26 @@
+// A - I
+document.image1.onmouseover = function() {
+	this.src = '../images/button-14-on.gif';
+}
+
+document.image1.onmouseout = function() {
+	this.src = '../images/button-14-off.gif';
+}
+
+document.form1.onsubmit = function() {
+	return submitMe(this);
+}
+
+document.form2.onsubmit = function() {
+	submitMe(this);
+}
+
+function submitMe(form)
+{
+	if (parseInt(form.box.value) % 2 == 0) {
+		return true;
+	}
+
+	alert("Not an even number");
+	return false;
+}
