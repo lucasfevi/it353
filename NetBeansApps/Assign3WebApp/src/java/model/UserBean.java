e781c5e5c6b95d10666d58675e2889625a6b5cf0
Lucas Vieira <newlucasfevi@gmail.com>
2016-03-24 12:54:30 -0500

    Added some validations

--- NetBeansApps/Assign3WebApp/src/java/model/UserBean.java
+++ NetBeansApps/Assign3WebApp/src/java/model/UserBean.java
@@ -4,6 +4,11 @@ import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ComponentSystemEvent;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -153,6 +158,35 @@ public class UserBean {
             + "<br><b>Security Answer:</b> " + this.getSecAnswer();
         
         return userInfo;
+    }
+    
+    public void validatePassword(ComponentSystemEvent event) {
+        FacesContext fc = FacesContext.getCurrentInstance();
+
+        UIComponent components = event.getComponent();
+
+        // get password
+        UIInput uiInputPassword = (UIInput) components.findComponent("password");
+        String password = uiInputPassword.getLocalValue() == null ? ""
+            : uiInputPassword.getLocalValue().toString();
+        String passwordId = uiInputPassword.getClientId();
+
+        // get confirm password
+        UIInput uiInputConfirmPassword = (UIInput) components.findComponent("confirmPassword");
+        String confirmPassword = uiInputConfirmPassword.getLocalValue() == null ? ""
+            : uiInputConfirmPassword.getLocalValue().toString();
+
+        // Let required="true" do its job.
+        if (password.isEmpty() || confirmPassword.isEmpty()) {
+            return;
+        }
+
+        if (!password.equals(confirmPassword)) {
+            FacesMessage msg = new FacesMessage("Password must match confirm password");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+            fc.addMessage(passwordId, msg);
+            fc.renderResponse();
+        }
     }
     
     public String getUserInfoAsHtml() {
